bd5c08f5da5d240e5482ec5e7d1ab7f13bf52e7d
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-08-03 21:38:47 +0430

    change App.js to class-base and some style changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
 import Calc from './components/calculator';
 
-function App() {
-  return (
-    <Calc />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <Calc />
+    );
+  }
 }
 
 export default App;
