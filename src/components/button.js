5fac817a32d5eb322f9b10fd97a711298d942e4f
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-08-06 02:15:57 +0430

    Add logic to the project

--- src/components/button.js
+++ src/components/button.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 class TempButton extends React.Component {
   render() {
     const {
-      firstButton, secondButton, thirdButton, fourthButton, updateObj
+      firstButton, secondButton, thirdButton, fourthButton, updateObject,
     } = this.props;
     return (
       <div className="row">
-        {firstButton ? <button type="button" className="calcButton" onClick={updateObj} buttonName={firstButton}>{firstButton}</button> : null}
-        {firstButton ? <button type="button" className="calcButton" onClick={updateObj} buttonName={secondButton}>{secondButton}</button> : <button type="button" className="zeroButton" onClick={updateObj} buttonName={secondButton}>{secondButton}</button>}
-        <button type="button" className="calcButton" onClick={updateObj} buttonName={thirdButton}>{thirdButton}</button>
-        <button type="button" className="calcButtonColor" onClick={updateObj} buttonName={thirdButton}>{fourthButton}</button>
+        {firstButton ? <button type="button" className="calcButton" onClick={updateObject}>{firstButton}</button> : null}
+        {firstButton ? <button type="button" className="calcButton" onClick={updateObject}>{secondButton}</button> : <button type="button" className="zeroButton" onClick={updateObject}>{secondButton}</button>}
+        <button type="button" className="calcButton" onClick={updateObject}>{thirdButton}</button>
+        <button type="button" className="calcButtonColor" onClick={updateObject}>{fourthButton}</button>
       </div>
     );
   }
@@ -26,7 +26,7 @@ TempButton.propTypes = {
   secondButton: PropTypes.string.isRequired,
   thirdButton: PropTypes.string.isRequired,
   fourthButton: PropTypes.string.isRequired,
-  updateObj: PropTypes.func.isRequired,
+  updateObject: PropTypes.func.isRequired,
 };
 
 export default TempButton;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,16 +1,59 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 import TempButton from './button';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataObject: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.updateObject = this.updateObject.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.updateScreen();
+  }
+
+  updateObject(event) {
+    const val = event.target.textContent;
+    this.setState((prevState) => ({
+      dataObject: { ...calculate(prevState.dataObject, val) },
+    }));
+  }
+
+  updateScreen() {
+    const screen = document.querySelector('.result-screen');
+    const { dataObject } = this.state;
+    if (dataObject.next && !dataObject.total) {
+      screen.value = dataObject.next;
+    } else if (!dataObject.next && dataObject.total) {
+      screen.value = dataObject.operation
+        ? `${dataObject.total} ${dataObject.operation}`
+        : dataObject.total;
+    } else if (!dataObject.next && !dataObject.total) {
+      screen.value = '';
+    } else {
+      screen.value = `${dataObject.total} ${dataObject.operation
+        ? `${dataObject.operation} ${dataObject.next}`
+        : dataObject.next
+      }`;
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <input type="text" className="input" placeholder="0" />
-        <TempButton firstButton="AC" secondButton="+/-" thirdButton="%" fourthButton="÷" />
-        <TempButton firstButton="7" secondButton="8" thirdButton="9" fourthButton="x" />
-        <TempButton firstButton="4" secondButton="5" thirdButton="6" fourthButton="-" />
-        <TempButton firstButton="1" secondButton="2" thirdButton="3" fourthButton="+" />
-        <TempButton secondButton="0" thirdButton="." fourthButton="=" />
+        <input type="text" className="result-screen" value={0} readOnly />
+        <TempButton firstButton="AC" secondButton="+/-" thirdButton="%" fourthButton="÷" updateObject={this.updateObject} />
+        <TempButton firstButton="7" secondButton="8" thirdButton="9" fourthButton="x" updateObject={this.updateObject} />
+        <TempButton firstButton="4" secondButton="5" thirdButton="6" fourthButton="-" updateObject={this.updateObject} />
+        <TempButton firstButton="1" secondButton="2" thirdButton="3" fourthButton="+" updateObject={this.updateObject} />
+        <TempButton secondButton="0" thirdButton="." fourthButton="=" updateObject={this.updateObject} />
       </div>
     );
   }
